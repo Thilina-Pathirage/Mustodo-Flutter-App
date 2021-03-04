16ca5a45310177254cf95270bd0ab3806561b9c6
Thilina-Pathirage <thilinapathirage@gmail.com>
2021-03-04 20:59:45 +0530

    welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -12,81 +12,99 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               Container(
-                width: 350.0,
-                margin: EdgeInsets.all(10.0),
-                child: ButtonTheme(
-                  minWidth: 200.0,
-                  height: 50.0,
-                  buttonColor: Colors.blue,
-                  child: RaisedButton(
-                    elevation: 10.0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
+                height: 500.0,
+                child: Image(
+                  width: 200.0,
+                  image: AssetImage('images/MusToDo.png'),
+                ),
+              ),
+             Container(
+               child: Column(
+                 children: <Widget>[
+                   Container(
+                     width: 300.0,
+                     margin: EdgeInsets.all(10.0),
+                     child: ButtonTheme(
+                       minWidth: 200.0,
+                       height: 60.0,
+                       buttonColor: Colors.blue,
+                       child: RaisedButton(
+                         elevation: 10.0,
+                         shape: RoundedRectangleBorder(
+                           borderRadius: BorderRadius.circular(22.0),
+                         ),
+                         onPressed: () {},
+                         child: Row(
+                           mainAxisSize: MainAxisSize.min,
+                           children: <Widget>[
+                             /*
+
                         Image.asset('images/mustodologilogo.png',
                           height: 30.0,
                           width: 30.0,
                         ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(30.0, 0, 50.0, 0),
-                          child: Text(
-                              "Login with Mustodo",
-                            style: TextStyle(fontWeight: FontWeight.bold,
-                              fontSize: 20.0
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    textColor: Colors.white,
-                  ),
-                ),
-              ),
 
-              Container(
-                width: 350.0,
-                margin: EdgeInsets.all(10.0),
-                child: ButtonTheme(
-                  minWidth: 200.0,
-                  height: 50.0,
-                  buttonColor: Colors.red,
-                  child: RaisedButton(
-                    elevation: 10.0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Image.asset('images/glogologinn.png',
+                         */
+                             Padding(
+                               padding: EdgeInsets.fromLTRB(0.0, 0, 0.0, 0),
+                               child: Text(
+                                 "Login with Mustodo",
+                                 style: TextStyle(fontWeight: FontWeight.bold,
+                                     fontSize: 15.0
+                                 ),
+                               ),
+                             ),
+                           ],
+                         ),
+                         textColor: Colors.white,
+                       ),
+                     ),
+                   ),
+                   Container(
+                     width: 300.0,
+                     margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 50.0),
+                     child: ButtonTheme(
+                       minWidth: 200.0,
+                       height: 60.0,
+                       buttonColor: Colors.redAccent,
+                       child: RaisedButton(
+                         elevation: 10.0,
+                         shape: RoundedRectangleBorder(
+                           borderRadius: BorderRadius.circular(22.0),
+                         ),
+                         onPressed: () {},
+                         child: Row(
+                           mainAxisSize: MainAxisSize.min,
+                           children: <Widget>[
+                             /*
+
+                        Image.asset('images/mustodologilogo.png',
                           height: 30.0,
                           width: 30.0,
                         ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(40.0, 0, 50.0, 0),
-                          child: Text(
-                            "Login with Google",
-                            style: TextStyle(fontWeight: FontWeight.bold,
-                                fontSize: 20.0
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    textColor: Colors.white,
-                  ),
-                ),
-              ),
+
+                         */
+                             Padding(
+                               padding: EdgeInsets.fromLTRB(0.0, 0, 0.0, 0),
+                               child: Text(
+                                 "Login with Google",
+                                 style: TextStyle(fontWeight: FontWeight.bold,
+                                     fontSize: 15.0
+                                 ),
+                               ),
+                             ),
+                           ],
+                         ),
+                         textColor: Colors.white,
+                       ),
+                     ),
+                   ),
+                 ],
+               ),
+             )
 
             ],
           ),
